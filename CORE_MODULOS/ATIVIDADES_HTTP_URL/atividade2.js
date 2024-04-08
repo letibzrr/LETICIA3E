4776a1defcd87584f0850ac6ce7e4eee07634415
Letícia Bezerra <138785221+letibzrr@users.noreply.github.com>
2024-04-08 11:04:44 -0300

    Delete CORE_MODULOS directory

--- CORE_MODULOS/ATIVIDADES_HTTP_URL/atividade2.js
+++
@@ -1,8 +0,0 @@
-const url = require('url')
-const adress = 'http://api.example.com/data?param1=value1&param2=value2'
-const parseUrl = new url.URL(adress)
-
-console.log(parseUrl.hostname)
-console.log(parseUrl.pathname)
-console.log(parseUrl.searchParams)
-
--- CORE_MODULOS/URL/index.js
+++
@@ -1,9 +0,0 @@
-const url = require('url')
-const adress = 'https://www.meusite.com.br/catalog?produtos=cadeira'
-const parseUrl = new url.URL(adress)
-
-console.log(parseUrl.host) // www.meusite.com.br
-console.log(parseUrl.pathname) // catalog
-console.log(parseUrl.search) // ?produtos=cadeira
-console.log(parseUrl.searchParams) // URLSearchParams { 'produtos' => 'cadeira' }
-console.log(parseUrl.searchParams.get('produtos')) // cadeira
