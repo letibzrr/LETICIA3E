f0a5ed8da07ca7d79a2435ee973f863bd829aab0
Letícia Bezerra <138785221+letibzrr@users.noreply.github.com>
2024-04-10 12:26:39 -0300

    Add files via upload

---
+++ front-end-PROJETO/react-hooks/src/Modal/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ButtonModal from "./ButtonModal";
+import Modal from "./Modal";
+
+const App = () => {
+
+    const [modal, setModal] = React.useState(false)
+
+    return(
+        <>
+            <ButtonModal setModal={setModal}/>
+            <Modal modal={modal} setModal={setModal}/>
+        </>
+    )
+}
+
+export default App;
---
+++ front-end-PROJETO/react-hooks/src/Modal/ButtonModal.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+const ButtonModal = ({setModal}) => {
+    return(
+        <button onClick={() => setModal(true)}>Abrir Modal</button>
+    )
+}
+
+export default ButtonModal;
