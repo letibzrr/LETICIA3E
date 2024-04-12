1bc13ddcbcedc2902f631ad65a45ea990ef69bc4
Letícia Bezerra <138785221+letibzrr@users.noreply.github.com>
2024-04-12 09:58:43 -0300

    API finalizada

--- back-end-CODIFICAÇÃO/APIs/1_HTTP_ARRAY/server.js
+++ back-end-CODIFICAÇÃO/APIs/1_HTTP_ARRAY/server.js
@@ -7,6 +7,7 @@ const PORT = 3333
 1- corpo da requisição (request.body) (POST)
 2- parâmetro de busca (search PARAMS, QUERY PARAMS) - http://localhost:3333/user/3 (GET)
 3- parâmetros de rota (ROUTE PARAMS) - http://localhost:3333/user/1 (PUT, PATH, DELETE)
+==> PROTOCOLO (http) ==> DOMINIO (localhost) ==> PORTA (3333) ==> ENDPOINT (ex: user)
 **/ 
 const users = []
 const server = http.createServer((request, response) => {
@@ -14,7 +15,17 @@ const server = http.createServer((request, response) => {
     if(url === '/users' && method === "GET"){ // buscar todos os usuários
         response.setHeader('Content-type', 'application/json')
         response.end(JSON.stringify(users)) // stringify = receber informações como JSON
-    }else if(false){ // buscar um único usuário
+    }else if(url.startsWith('/users/') && method === 'GET'){ // buscar um único usuário
+        // startsWith = validação de todos os dados
+        const userId = url.split('/')[2] // split = manipulação de dados por pedaços
+        const user = users.find((user) => user.id == userId) // encontrar atraves do 'find'
+        if(user){
+            response.setHeader("Content-Type", "application/json")
+            response.end(JSON.stringify(user))
+        }else{
+            response.writeHead(404, {"Content-Type": "application/json"})
+            response.end(JSON.stringify({ message: "Usuário não encontrado!" }))
+        }
     }else if(url === '/users' && method === "POST"){ // cadastrar um usuário
         let body = ''
         request.on('data', (chunk) => {
@@ -22,14 +33,44 @@ const server = http.createServer((request, response) => {
         })
         request.on('end', () => {
             const newUser = JSON.parse(body)
-            newUser.id = '1'
+            newUser.id = users.length + 1
             users.push(newUser)
-            response.writeHead(201, {'Content-type': 'application/json'})
+            response.writeHead(201, {'Content-Type': 'application/json'})
             response.end(JSON.stringify(newUser))
         })
-    }else if(true){ // atualizar um usuário
-    }else if(true){ // deletar um usuário
+    }else if(url.startsWith('/users/') && method === 'PUT'){ // atualizar um usuário
+        const userId = url.split('/')[2]
+        let body = ''
+        request.on('data', (chunk) => {
+            body += chunk.toString() 
+        })
+        request.on('end', () => {
+            const uptadeUser = JSON.parse(body)
+            const index = users.findIndex((user) => user.id == userId)
+            if(index !== -1){
+                users[index] = {...users[index], uptadeUser} // reatribuição e junção 
+                response.setHeader('Content-Type', 'application/json')
+                response.end(JSON.stringify(user[index]))
+            }else{
+                response.writeHead(404, {'Content-Type': 'application/json'})
+                response.end(JSON.stringify({ message: "Erro ao atualizar as informações"}))
+            }
+        })
+        const user = users.find((user) => user.id == userId) 
+    }else if(url.startsWith('/users/') && method === 'DELETE'){ // deletar um usuário
+        const userId = url.split('/')[2]
+        const index = users.findIndex((user) => user.id == userId)
+            if(index !== -1){
+                users.splice(index, 1)
+                response.setHeader('Content-Type', 'application/json')
+                response.end(JSON.stringify({ message: "Usuário excluido" }))
+            }else{
+                response.writeHead(404, {'Content-Type': 'application/json'})
+                response.end(JSON.stringify({ message: "Erro ao tentar excluir usuário" }))
+            }
     }else{ // recurso não encontrado
+        response.writeHead(404, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify({ message: "Página não encontrada"}))
     }
 })
 server.listen(PORT, () => {
