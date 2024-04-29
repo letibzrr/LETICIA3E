3c20b95f795fba2ad60b2cf23f3226dde8f34e4d
Letícia Bezerra <138785221+letibzrr@users.noreply.github.com>
2024-04-29 10:54:10 -0300

    atualizações api http fs

--- back-end-CODIFICAÇÃO/APIs/3_HTTP_FS_ATV/server.js
+++ back-end-CODIFICAÇÃO/APIs/3_HTTP_FS_ATV/server.js
@@ -1,87 +1,106 @@
 import http from 'node:http'
-const PORT = 3333
+import fs from 'node:fs'
 
-const empregados = []
+const PORT = 3333
 
 const server = http.createServer((request, response) => {
     const {method, url} = request
-    if(url === '/empregados' && method === "GET"){ // listar todos os funcionários cadastrados
-        response.setHeader('Content-type', 'application/json')
-        response.end(JSON.stringify(empregados)) 
-    }else if(url.startsWith('/empregados/count') && method === "GET"){ // contar o número total de funcionários cadastrados
-        let numeroTotalEmpregados = empregados.length;
-        response.writeHead(201, {'Content-Type':'aplication/json'})
-        return response.end(JSON.stringify({message: 'Quantidade total de empregados', numeroTotalEmpregados}))
-    }else if(url.startsWith('/empregados/porCargo/{cargo}') && method === "GET"){ // listar todos os funcionários de um determinado cargo
-        // ???
-    }else if(url.startsWith('/empregados/porHabilidade/{habilidade}') && method === "GET"){ // listar todos os funcionários que possuam uma determinada habilidade
-        // ???
-    }else if(url.startsWith('/empregados/porFaixaSalarial?min={min}&max={max}') && method === "GET"){ // listar todos os funcionários dentro de uma faixa salarial especificada
-        // ???
-    }else if(url.startsWith('/empregados/') && method === "GET"){ // detalhes de um funcionário específico com base em seu ID
-        const empregadoID = url.split('/')[2]
-        const findEmpregados = empregados.find((empregado) => {
-            return empregado.id == empregadoID
-        })
-        if(!findEmpregados){
-            response.writeHead(403, {'Content-Type':'application/json'})
-            return response.end(JSON.stringify({message: 'empregado não encontrado'}))
+    
+    fs.readFile("empregados.json", 'utf8', (err, data) => {
+        if(err){
+            response.writeHead(500, {'Content-Type':'application/json'})
+            response.end(JSON.stringify({message: 'Erro ao buscar os dados'}))
+            return;
         }
-        response.setHeader('Content-Type', 'application/json')
-        response.end(JSON.stringify(findEmpregados))
-    }else if(url.startsWith('/empregados') && method === "POST"){ // cadastrar um novo funcionário
-        let body = ''
-        request.on('data', (chunk) => {
-            body += chunk
+        let jsonData = [];
+        try{
+            jsonData = JSON.parse(data)
+        }catch(error){
+            console.error('Erro ao ler o arquivo jsonData'+error)
+        }
+        if(url === '/empregados' && method === "GET"){ // listar todos os funcionários cadastrados
+            fs.readFile('empregados.json', 'utf8', (err, data) => {
+                if(err){
+                    response.writeHead(500, {'Content-Type':'application/json'})
+                    response.end(JSON.stringify({message: 'Erro ao buscar os dados'}))
+                    return; 
+                }
+                const jsonData = JSON.parse(data)
+                response.writeHead(200, {'Content-Type':'application/json'})
+                response.end(JSON.stringify(jsonData))
+            })
+        }else if(url === '/empregados/count' && method === "GET"){ // contar o número total de funcionários cadastrados
+            fs.readFile('empregados.json', 'utf8', (err, data) => {
+                if(err){
+                    response.writeHead(500, {'Contet-Type':'application/json'})
+                    response.end(JSON.stringify({message: 'Erro ao ler o arquivo'}))
+                }
+                const jsonData = JSON.parse(data)
+                const totalEmpregados = jsonData.length
+
+                response.writeHead(200, {'Content-Type':'application/json'})
+                response.end(JSON.stringify({message: `Total de empregados é ${totalEmpregados}`}))
+            })
+        }else if(url.startsWith('/empregados/porCargo/{cargo}') && method === "GET"){ // listar todos os funcionários de um determinado cargo
+            console.log('GET /empregados/porCargo/{cargo}')
+            response.end()
+        }else if(url.startsWith('/empregados/porHabilidade/{habilidade}') && method === "GET"){ // listar todos os funcionários que possuam uma determinada habilidade
+            console.log('GET /empregados/porHabilidade/{habilidade}')
+            response.end()
+        }else if(url.startsWith('/empregados/porFaixaSalarial?min={min}&max={max}') && method === "GET"){ // listar todos os funcionários dentro de uma faixa salarial especificada
+            console.log('GET /empregados/porFaixaSalarial?min={min}&max={max}')
+            response.end()
+        }else if(url.startsWith('/empregados/') && method === "GET"){ // detalhes de um funcionário específico com base em seu ID
+            const id = parseInt(url.split('/')[2])
+            fs.readFile('empregados.json', 'utf8', (err) => {
+                if(err){
+                    response.writeHead(500, {'Contet-Type':'application/json'})
+                    response.end(JSON.stringify({message: 'Erro ao ler o arquivo'}))
+                }
+                const jsonData = JSON.parse(data)
+                const indexEmpregado = jsonData.findIndex((empregado) => empregado.id === id)
+
+                if(indexEmpregado === -1){
+                    response.writeHead(404, {'Content-Type':'application/json'})
+                    response.end(JSON.stringify({message: 'Usuário não encontrado'}))
+                    return
+                }
+                const empregadoEncontrado = jsonData[indexEmpregado]
+                response.writeHead(200, {'Content-Type':'application/json'})
+                response.end(JSON.stringify(empregadoEncontrado))
+            })
+        }else if(url === '/empregados' && method === "POST"){ // cadastrar um novo funcionário
+            let body = ''
+            request.on('data', (chunk) => {
+                body += chunk
+            })
+            request.on('end', () => {
+                const novoEmpregado = JSON.parse(body)
+                novoEmpregado.id = jsonData.length + 1
+                jsonData.push(novoEmpregado)
+
+                fs.writeFile("empregados.json", JSON.stringify(jsonData, null, 2), (err) => {
+                    if(err){
+                        response.writeHead(500, {'Content-Type':'application/json'})
+                        response.end(JSON.stringify({message: 'Erro interno no servidor'}))
+                        return
+                    }
+                    response.writeHead(201, {'Content-Type':'application/json'})
+                    response.end(JSON.stringify(novoEmpregado))
+                })
         })
-        request.on('end', () => {
-            const empregado = JSON.parse(body)
-            if(empregado.idade < 18){
-                response.writeHead(403, {'Content-Type':'aplication/json'})
-                return response.end(JSON.stringify({message: 'Sua idade não é permitida!'}))
-            }else if(empregado.senha !== empregado.confirmaSenha){
-                response.writeHead(400, {'Content-Type':'aplication/json'})
-                return response.end(JSON.stringify({message: 'Senha incorreta!'}))
-            }
-            empregado.id = empregados.length + 1
-            empregados.push(empregado)
-            response.writeHead(201, {'Content-Type':'aplication/json'})
-            response.end(JSON.stringify(empregado))
-        })
-    }else if(url.startsWith('/empregados/') && method === "PUT"){ // atualizar as informações de um funcionário específico com base em seu ID
-        const empregadoID = url.split('/')[2]
-        let body = ''
-        request.on('data', (chunk) => {
-            body += chunk
-        })
-        request.on('end', () => {
-            const updateEmpregado = JSON.parse(body)
-            const index = empregados.findIndex(empregado => empregado.id == empregadoID)
-            if(index !== -1){
-                empregados[index] = {...empregados[index], ...updateEmpregado}
-                response.setHeader('Content-TYpe','application/json')
-                return response.end(JSON.stringify(empregados[index]))
-            }else{
-                response.writeHead(404, {'Content-Type':'application/json'})
-                return response.end(JSON.stringify({message: 'empregado não encontrado'}))
-            }
-        })
-    }else if(url.startsWith('/empregados/') && method === "DELETE"){ // excluir um funcionário específico com base em seu ID
-        const empregadoID = url.split('/')[2]
-        const index = empregados.findIndex((empregado) => empregado.id == empregadoID)
-            if(index !== -1){
-                empregados.splice(index, 1)
-                response.setHeader('Content-Type', 'application/json')
-                response.end(JSON.stringify({ message: 'empregado deletado' }))
-            }else{
-                response.writeHead(404, {'Content-Type': 'application/json'})
-                response.end(JSON.stringify({ message: 'empregado não encontrado' }))
-            }
-    }else{ // rota de página não encontrada
-        response.writeHead(404, {'Content-Type': 'application/json'})
-        response.end(JSON.stringify({codigo: 404, message: "página não encontrada!"}))
-    }
-})
+        }else if(url.startsWith('/empregados/') && method === "PUT"){ // atualizar as informações de um funcionário específico com base em seu ID
+            console.log('PUT / empregados')
+            response.end()
+        }else if(url.startsWith('/empregados/') && method === "DELETE"){ // excluir um funcionário específico com base em seu ID
+            console.log('DELETE / empregados')
+            response.end()
+        }else{ // rota de página não encontrada
+            response.writeHead(404, {'Content-Type': 'application/json'})
+            response.end(JSON.stringify({codigo: 404, message: "Página não encontrada"}))
+        }
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`Servidor on PORT: ${PORT}`)
