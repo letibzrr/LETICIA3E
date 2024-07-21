7d89c5306e4c0e7ba34d53b4c5c786f0a8b7b9c4
Letícia Bezerra <138785221+letibzrr@users.noreply.github.com>
2024-07-21 20:55:05 -0300

    LABSTOCK - teste equipamentos

---
+++ front-end-PROJETO/teste_equipamentos/src/Styles/Equipamento.js
@@ -0,0 +1,182 @@
+import styled from 'styled-components'
+
+const Cores = {
+    Branco: '#ffff',
+    Verde: '#2D8034',
+    Marrom: '#3A3132'
+}
+
+export const Container = styled.section`
+`
+
+export const TituloEquipamento = styled.h1`
+`
+export const DescricaoEquipamento = styled.p`
+`
+
+
+export const Box01 = styled.div`
+`
+export const Box02 = styled.div`
+`
+export const Box03 = styled.div`
+`
+export const Box04 = styled.div`
+`
+export const Box05 = styled.div`
+`
+
+export const CardEq01 = styled.section`
+`
+export const CardEq02 = styled.section`
+`
+export const CardEq03 = styled.section`
+`
+export const CardEq04 = styled.section`
+`
+export const CardEq05 = styled.section`
+`
+export const CardEq06 = styled.section`
+`
+export const CardEq07 = styled.section`
+`
+export const CardEq08 = styled.section`
+`
+export const CardEq09 = styled.section`
+`
+export const CardEq10 = styled.section`
+`
+export const CardEq11 = styled.section`
+`
+export const CardEq12 = styled.section`
+`
+export const CardEq13 = styled.section`
+`
+export const CardEq14 = styled.section`
+`
+export const CardEq15 = styled.section`
+`
+
+export const TituloEq01 = styled.h1`
+`
+export const TituloEq02 = styled.h1`
+`
+export const TituloEq03 = styled.h1`
+`
+export const TituloEq04 = styled.h1`
+`
+export const TituloEq05 = styled.h1`
+`
+export const TituloEq06 = styled.h1`
+`
+export const TituloEq07 = styled.h1`
+`
+export const TituloEq08 = styled.h1`
+`
+export const TituloEq09 = styled.h1`
+`
+export const TituloEq10 = styled.h1`
+`
+export const TituloEq11 = styled.h1`
+`
+export const TituloEq12 = styled.h1`
+`
+export const TituloEq13 = styled.h1`
+`
+export const TituloEq14 = styled.h1`
+`
+export const TituloEq15 = styled.h1`
+`
+
+export const DescricaoEq01 = styled.p`
+`
+export const DescricaoEq02 = styled.p`
+`
+export const DescricaoEq03 = styled.p`
+`
+export const DescricaoEq04 = styled.p`
+`
+export const DescricaoEq05 = styled.p`
+`
+export const DescricaoEq06 = styled.p`
+`
+export const DescricaoEq07 = styled.p`
+`
+export const DescricaoEq08 = styled.p`
+`
+export const DescricaoEq09 = styled.p`
+`
+export const DescricaoEq10 = styled.p`
+`
+export const DescricaoEq11 = styled.p`
+`
+export const DescricaoEq12 = styled.p`
+`
+export const DescricaoEq13 = styled.p`
+`
+export const DescricaoEq14 = styled.p`
+`
+export const DescricaoEq15 = styled.p`
+`
+
+export const Disponibilidadeeq01 = styled.section`
+`
+export const Disponibilidadeeq02 = styled.section`
+`
+export const Disponibilidadeeq03 = styled.section`
+`
+export const Disponibilidadeeq04 = styled.section`
+`
+export const Disponibilidadeeq05 = styled.section`
+`
+export const Disponibilidadeeq06 = styled.section`
+`
+export const Disponibilidadeeq07 = styled.section`
+`
+export const Disponibilidadeeq08 = styled.section`
+`
+export const Disponibilidadeeq09 = styled.section`
+`
+export const Disponibilidadeeq10 = styled.section`
+`
+export const Disponibilidadeeq11 = styled.section`
+`
+export const Disponibilidadeeq12 = styled.section`
+`
+export const Disponibilidadeeq13 = styled.section`
+`
+export const Disponibilidadeeq14 = styled.section`
+`
+export const Disponibilidadeeq15 = styled.section`
+`
+
+export const ImgEquipamento1 = styled.img`
+`
+export const ImgEquipamento2 = styled.img`
+`
+export const ImgEquipamento3 = styled.img`
+`
+export const ImgEquipamento4 = styled.img`
+`
+export const ImgEquipamento5 = styled.img`
+`
+export const ImgEquipamento6 = styled.img`
+`
+export const ImgEquipamento7 = styled.img`
+`
+export const ImgEquipamento8 = styled.img`
+`
+export const ImgEquipamento9 = styled.img`
+`
+export const ImgEquipamento10 = styled.img`
+`
+export const ImgEquipamento11 = styled.img`
+`
+export const ImgEquipamento12 = styled.img`
+`
+export const ImgEquipamento13 = styled.img`
+`
+export const ImgEquipamento14 = styled.img`
+`
+export const ImgEquipamento15 = styled.img`
+`
